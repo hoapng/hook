8a2dba40d4064cd66674297e86db71483a2cbce6
hoapng <giahoa01giathieu@gmail.com>
2023-07-04 13:22:27 +0700

    State và useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './view/Nav';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('Hoa');
+  const [address, setAddress] = useState('');
+  const handleEventClick = (event) => {
+    setName(address)
+  }
+  const handleOnChange = (event) =>{
+    setAddress(event.target.value);
+    console.log(event.target.value)
+  }
   return (
+    
     <div className="App">
       <Nav></Nav>
+      <div>{name} - {address}</div>
+      <input type="text" value={address} onChange={(event) => handleOnChange(event)} />
+      <button type="button" onClick={(event) => handleEventClick(event)}>Click me</button>
     </div>
   );
 }
--- src/view/Nav.js
+++ src/view/Nav.js
@@ -1,8 +1,8 @@
 import './Nav.css'
 const Nav = ()=>{
     return(
-        <div class="topnav">
-            <a class="active" href="#home">Home</a>
+        <div className="topnav">
+            <a className="active" href="#home">Home</a>
             <a href="#news">News</a>
             <a href="#contact">Contact</a>
             <a href="#about">About</a>
