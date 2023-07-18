7a6116147f0eb40f3a7ea91bc408c20a91e028c5
hoapng <giahoa01giathieu@gmail.com>
2023-07-18 20:01:20 +0700

    prop

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
-import Nav from './view/Nav';
+import Nav from './view/Nav'; 
 import { useState } from 'react';
+import Todo from './view/Todo';
 
-function App() {
-  const [name, setName] = useState('Hoa');
+
+// template + logic
+// JSX
+//babel
+
+const App = () => { //class
+
+  //state
+  let [name, setName] = useState('Eric'); //[a1, b1, c1....]
   const [address, setAddress] = useState('');
-  const [todo, setTodo] = useState([
-    {id: '1', value: 'homework'}
+  const [todos, setTodos] = useState([
+    { id: 'todo1', title: 'Watching Hoi Dan IT Channel' },
+    { id: 'todo2', title: 'Doing homework' },
+    { id: 'todo3', title: 'Playing game' }
   ]);
+
+
   const handleEventClick = (event) => {
-    if(!address){
-      alert('empty');
+    if (!address) {
+      alert('emtpy input')
       return;
     }
-    let newTodo = { id: Math.random(), value: address}
-    setTodo([...todo, newTodo])
+    //hook not merge state
+    //...spread syntax array js
+    let newTodo = { id: 'abc', title: address }
+    setTodos([...todos, newTodo])
     setAddress('')
   }
-  const handleOnChange = (event) =>{
-    setAddress(event.target.value);
-    console.log(event.target.value)
+
+  const handleOnchangeInput = (event) => {
+    setAddress(event.target.value)
   }
+  //re-render
+  //for for-each => map
   return (
-    
     <div className="App">
-      <Nav></Nav>
-      <div>{name}</div>
-      <input type="text" value={address} onChange={(event) => handleOnChange(event)} />
-      <button type="button" onClick={(event) => handleEventClick(event)}>Click me</button>
-      <div className='todo-container'>
-        {todo&&todo.length>0&&todo.map(todo=>{
-          return(
-            <li className='todo-child' key={todo.id}>{todo.value}</li>
-          )
-        })}
-      </div>
+      <Nav />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello world with React and {name}!</h1>
+        <Todo
+          todos={todos}
+          title={'All todos'}
+        />
+        <input type="text" value={address} onChange={(event) => handleOnchangeInput(event)} />
+        <button type="button" onClick={(event) => handleEventClick(event)}>Click me</button>
+      </header>
     </div>
   );
 }
--- src/view/Nav.js
+++ src/view/Nav.js
@@ -1,12 +1,15 @@
-import './Nav.css'
-const Nav = ()=>{
-    return(
+import './Nav.scss'
+
+
+const Nav = () => {
+    return (
         <div className="topnav">
             <a className="active" href="#home">Home</a>
             <a href="#news">News</a>
             <a href="#contact">Contact</a>
             <a href="#about">About</a>
         </div>
-    )
+    );
 }
-export default Nav
+
+export default Nav;
