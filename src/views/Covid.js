785e2e0905219f268ed0ec87b478843f3ae288ce
hoapng <giahoa01giathieu@gmail.com>
2023-07-30 11:02:30 +0700

    Conditional Loading Message

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -5,46 +5,57 @@ import moment from 'moment';
 const Covid = () => {
 
     const [dataCovid, setDataCovid] = useState([]);
+    const [loading, setLoading] = useState(true)
     // componentDidMount
     useEffect(async () => {
-        let res = await axios.get('https://api.covid19api.com/country/vietnam?from=2021-10-01T00:00:00Z&to=2021-10-20T00:00:00Z')
-        let data = res && res.data ? res.data : [];
-        if (data && data.length > 0) {
-            data.map(item => {
-                item.Date = moment(item.Date).format('DD/MM/YYYY');
-                return item;
-            })
-        }
-        setDataCovid(data)
+        setTimeout(async () => {
+            let res = await axios.get('https://reqres.in/api/users?page=2')
+            let data = res && res.data ? res.data : [];
+            // if (data && data.length > 0) {
+            //     data.map(item => {
+            //         item.Date = moment(item.Date).format('DD/MM/YYYY');
+            //         return item;
+            //     })
+            // }
+            setDataCovid(data.data);
+            setLoading(false);
+        }, 3000)
     }, []);
 
     return (
         <>
             <h3>Covid 19 tracking in VietNam:</h3>
             <table>
+                {console.log('check data covid:', dataCovid)}
                 <thead>
                     <tr>
-                        <th>Date</th>
-                        <th>Confirmed</th>
-                        <th>Active</th>
-                        <th>Deaths</th>
-                        <th>Recovered</th>
+                        <th>id</th>
+                        <th>email</th>
+                        <th>first_name</th>
+                        <th>last_name</th>
+                        <th>avatar</th>
                     </tr>
                 </thead>
                 <tbody>
 
-                    {dataCovid && dataCovid.length > 0 &&
+                    {loading === false && dataCovid && dataCovid.length > 0 &&
                         dataCovid.map(item => {
                             return (
-                                <tr key={item.ID}>
-                                    <td>{item.Date}</td>
-                                    <td>{item.Confirmed}</td>
-                                    <td>{item.Active}</td>
-                                    <td>{item.Deaths}</td>
-                                    <td>{item.Recovered}</td>
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.first_name}</td>
+                                    <td>{item.last_name}</td>
+                                    <td>{item.avatar}</td>
                                 </tr>
                             )
                         })
+                    }
+
+                    {loading === true
+                        && <tr >
+                            <td colSpan='5' style={{ 'textAlign': 'center' }}>  Loading...</td>
+                        </tr>
                     }
 
                 </tbody>
